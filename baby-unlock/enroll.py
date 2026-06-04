73a2bdd99a6338000113892b5305d81b04d997db
cybugg <timehinsamuel11@gmail.com>
2026-06-04 19:58:33 +0100

    feat(baby-unclock): add enrollment script to capture audio

---
+++ baby-unlock/enroll.py
@@ -0,0 +1,14 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+SAMPLE_RATE = 16000
+DURATION = 4  # seconds
+
+print("Speak your enrollment phrase: 'hey baby unlock me'")
+
+audio = sd.rec(int(DURATION * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1)
+sd.wait()
+
+write("enroll.wav", SAMPLE_RATE, audio)
+
+print("Saved enrollment audio -> enroll.wav")
